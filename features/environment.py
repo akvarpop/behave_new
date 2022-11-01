6aee864b0f42864cbdf0b9d60aba6b8f7d2781db
Popravka <104668775+akvarpop@users.noreply.github.com>
2022-11-01 20:43:27 +0200

    headless

--- features/environment.py
+++ features/environment.py
@@ -4,7 +4,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 # options Chrome
 options = webdriver.ChromeOptions()
-options.add_argument('headless')
+#options.add_argument('headless')
 options.add_argument('--ignore-ssl-errors=yes')
 options.add_argument('--ignore-certificate-errors')
 
@@ -12,11 +12,12 @@ options.add_argument('--ignore-certificate-errors')
 def before_all(context):
     #Work on MAC without Docker
     #context.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-    #On Jenkins vs Docker
+    #On Jenkins vs Docker (not work for me)
     context.driver = webdriver.Remote(
         command_executor=f'http://localhost:4444/wd/hub',
         options=options
     )
+
     context.driver.maximize_window()
     context.driver.implicitly_wait(10)
 
